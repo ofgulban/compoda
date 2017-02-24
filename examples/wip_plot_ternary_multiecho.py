78818f3b9a134af7e06489fa69186eb6d5f9fc6b
ofgulban <farukgulban@gmail.com>
2017-02-24 23:57:54 +0100

    Load npy to do centering and standardization.

--- examples/wip_plot_ternary_multiecho.py
+++ examples/wip_plot_ternary_multiecho.py
@@ -5,15 +5,15 @@ import ternary
 import numpy as np
 import matplotlib.pyplot as plt
 from nibabel import load
-from tetrahydra.utils import truncate_and_scale
+from tetrahydra.core import closure, perturbation, powering
 np.seterr(divide='ignore', invalid='ignore')
 
 # Load data
-nii1 = load('/media/Data_Drive/Benchmark_Data/compositional_data/Benedikt/nifti/echo_1.nii.gz')
-nii2 = load('/media/Data_Drive/Benchmark_Data/compositional_data/Benedikt/nifti/echo_2.nii.gz')
-nii3 = load('/media/Data_Drive/Benchmark_Data/compositional_data/Benedikt/nifti/echo_3.nii.gz')
+nii1 = load('/home/faruk/Data/benedikt/echo_1.nii.gz')
+nii2 = load('/home/faruk/Data/benedikt/echo_2.nii.gz')
+nii3 = load('/home/faruk/Data/benedikt/echo_3.nii.gz')
 
-nii4 = load('/media/Data_Drive/Benchmark_Data/compositional_data/Benedikt/nifti/bet_mask_ero10.nii.gz')
+nii4 = load('/home/faruk/Data/benedikt/mostly_gm.nii.gz')
 msk = nii4.get_data().flatten()
 msk = msk > 0
 
@@ -21,33 +21,41 @@ basename = nii1.get_filename().split(os.extsep, 1)[0]
 dirname = os.path.dirname(nii1.get_filename())
 
 nr_measurements = nii1.shape[3]
-v=0
+
+# load descriptives for centering and standardization
+descriptives = np.load('/home/faruk/Data/benedikt/gm_descriptives.npy')
+descriptives = descriptives.item()
+center = descriptives["Center"]
+print "Sample center: " + str(center)
+totvar = descriptives['Total variance']
+print "Total variance: " + str(totvar)
+
 for v in range(nr_measurements):
     vol1 = nii1.get_data()[..., v]
     vol2 = nii2.get_data()[..., v]
     vol3 = nii3.get_data()[..., v]
     shape = vol1.shape + (3,)
 
-    # # normalize
-    # vol1 = truncate_and_scale(vol1, percMin=0, percMax=100)
-    # vol2 = truncate_and_scale(vol2, percMin=0, percMax=100)
-    # vol3 = truncate_and_scale(vol3, percMin=0, percMax=100)
+    comp = np.zeros(shape)
+    comp[:, :, :, 0] = vol1
+    comp[:, :, :, 1] = vol2
+    comp[:, :, :, 2] = vol3
+    comp = comp.reshape(shape[0]*shape[1]*shape[2], shape[3])
+    comp = comp[msk, :]  # apply mask
 
-    rgb = np.zeros(shape)
-    rgb[:, :, :, 0] = vol1
-    rgb[:, :, :, 1] = vol2
-    rgb[:, :, :, 2] = vol3
-    rgb = rgb.reshape(shape[0]*shape[1]*shape[2], shape[3])
-    rgb = rgb[msk, :]  # apply mask
-
+    # Impute
+    comp[comp == 0] = 1
     # Closure
-    sum_rgb = np.sum(rgb, axis=1)
-    for i in range(shape[3]):
-        rgb[:, i] = rgb[:, i]/sum_rgb
+    comp = closure(comp)
+    # Center
+    temp = np.ones(comp.shape) * center
+    comp = perturbation(comp, temp**-1)
+    # Standardize
+    comp = powering(comp, np.power(totvar, -1./2.))
 
     # Scale data if needed
-    scale = 100
-    rgb = rgb * scale
+    scale = 80
+    comp = comp * scale
 
     # Heatmap related
     start = 0
@@ -61,13 +69,13 @@ for v in range(nr_measurements):
     counts = []
     for i in range(len(a)):  # for every vertex
         # for j in range(len(a[i])):  # for every index in a vertex
-        idx_range_min = ((rgb[:, 0] > a[i][0]-0.5) &
-                         (rgb[:, 1] > a[i][1]-0.5) &
-                         (rgb[:, 2] > a[i][2]-0.5))
+        idx_range_min = ((comp[:, 0] > a[i][0]-0.5) &
+                         (comp[:, 1] > a[i][1]-0.5) &
+                         (comp[:, 2] > a[i][2]-0.5))
 
-        idx_range_max = ((rgb[:, 0] < a[i][0]+0.5) &
-                         (rgb[:, 1] < a[i][1]+0.5) &
-                         (rgb[:, 2] < a[i][2]+0.5))
+        idx_range_max = ((comp[:, 0] < a[i][0]+0.5) &
+                         (comp[:, 1] < a[i][1]+0.5) &
+                         (comp[:, 2] < a[i][2]+0.5))
         idx_range = (idx_range_min & idx_range_max)
         counts.append(np.log((idx_range).sum()))
 
@@ -88,6 +96,6 @@ for v in range(nr_measurements):
     tax.gridlines(multiple=scale/4., color="white")
     tax.heatmap(c, scale, style="hexagonal", vmin=0, vmax=5)
     # tax.show()
-    tax.savefig(os.path.join(dirname, 'plots_mostlywmcsf/test'+str(v).zfill(3)+'.png'))
+    tax.savefig(os.path.join(dirname, 'mostlygm/'+str(v).zfill(3)+'.png'))
     plt.close(figure)  # clear figure
     print str(v+1)+'/'+str(nr_measurements)
