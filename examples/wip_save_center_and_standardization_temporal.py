48586c07270c5ba625cc6469624391a6d2ac5a24
ofgulban <farukgulban@gmail.com>
2017-02-27 20:04:37 +0100

    Some more script testing stuff.

--- examples/wip_save_center_and_standardization_temporal.py
+++ examples/wip_save_center_and_standardization_temporal.py
@@ -5,7 +5,6 @@ import numpy as np
 from nibabel import load
 from tetrahydra.core import (closure, perturbation,
                              sample_center, sample_total_variance)
-from tempfile import TemporaryFile
 
 # Load data
 nii1 = load('/home/faruk/Data/benedikt/multi_echo_epi/nifti/echo_1.nii.gz')
@@ -22,29 +21,30 @@ dirname = os.path.dirname(nii1.get_filename())
 nr_measurements = nii1.shape[3]
 
 t = 0  # timepoint to be used in centering and standardization
-vol1 = nii1.get_data()[..., t]
-vol2 = nii2.get_data()[..., t]
-vol3 = nii3.get_data()[..., t]
+vol1 = nii1.get_data()
+vol2 = nii2.get_data()
+vol3 = nii3.get_data()
 shape = vol1.shape + (3,)
 
 comp = np.zeros(shape)
-comp[:, :, :, 0] = vol1
-comp[:, :, :, 1] = vol2
-comp[:, :, :, 2] = vol3
-comp = comp.reshape(shape[0]*shape[1]*shape[2], shape[3])
+comp[..., 0] = vol1
+comp[..., 1] = vol2
+comp[..., 2] = vol3
+comp = comp.reshape(shape[0]*shape[1]*shape[2], shape[3], shape[4])
 comp = comp[msk, :]  # apply mask
+traj = comp[1, :, :]  # select only one voxel for trajectory
 
 # Impute
-comp[comp == 0] = 1
+traj[traj == 0] = 1
 # Closure
-comp = closure(comp)
+traj = closure(traj)
 # Centering
-center = sample_center(comp)
+center = sample_center(traj)
 print "Sample center: " + str(center)
-temp = np.ones(comp.shape) * center
-comp = perturbation(comp, temp**-1)
+temp = np.ones(traj.shape) * center
+traj = perturbation(traj, temp**-1)
 # Total variance is used in standardization in the loop
-totvar = sample_total_variance(comp, center)
+totvar = sample_total_variance(traj, center)
 print "Total variance: " + str(totvar)
 
 # saving
