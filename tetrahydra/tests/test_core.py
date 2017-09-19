bd166b623a3a92c1ae21bc3e8a091b68dcba089a
ofgulban <farukgulban@gmail.com>
2017-09-19 19:30:09 +0200

    Test cosmetics.

--- tetrahydra/tests/test_core.py
+++ tetrahydra/tests/test_core.py
@@ -7,18 +7,33 @@ from tetrahydra.core import closure, perturb, power
 
 def test_closure():
     """Test closure operator."""
+    # Given
     data = np.random.random([2, 3])
-    assert np.sum(closure(data), axis=1) == pytest.approx(np.ones(2))
+    expected = np.ones(2)
+    # When
+    output = np.sum(closure(data), axis=1)
+    # Then
+    assert output == pytest.approx(expected)
 
 
 def test_perturb():
     """Test perturbation operator."""
+    # Given
     data = np.random.random([2, 3])
     p_vals = np.array([1., 2., 3.])  # perturbation values
-    assert perturb(data, p_vals, reclose=False) == pytest.approx(data * p_vals)
+    expected = data * p_vals
+    # When
+    output = perturb(data, p_vals, reclose=False)
+    # Then
+    assert np.all(output == expected)
 
 
 def test_power():
     """Test powering operator."""
+    # Given
     data = np.random.random([2, 3])
-    assert power(data, np.pi, reclose=False) == pytest.approx(data**np.pi)
+    expected = data**np.pi
+    # When
+    output = power(data, np.pi, reclose=False)
+    # Then
+    assert np.all(output == expected)
