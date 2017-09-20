37762c8977c36031452929660fcbf8f2e3b63bab
ofgulban <farukgulban@gmail.com>
2017-09-20 21:22:05 +0200

    Redundant lines removed.

--- tetrahydra/future/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Functions which are not yet well integrated stays in future submodule."""
-
-from simplex_coordinates1 import simplex_coordinates1
-from simplex_coordinates2 import simplex_coordinates2
