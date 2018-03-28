e19bc2fdbae2cf540266ac3d0a63250b1126e8c3
ofgulban <farukgulban@gmail.com>
2018-03-28 20:47:59 +0200

    Improve/cleanup example script.

--- examples/conversions/rgb_to_aitchison_metrics.py
+++ examples/conversions/rgb_to_aitchison_metrics.py
@@ -6,9 +6,9 @@ from nibabel import load, save, Nifti1Image
 import compoda.core as coda
 
 # Load data
-nii1 = load('/home/faruk/Data/Faruk/rgb_hsi/T1.nii.gz')
-nii2 = load('/home/faruk/Data/Faruk/rgb_hsi/PD.nii.gz')
-nii3 = load('/home/faruk/Data/Faruk/rgb_hsi/T2s.nii.gz')
+nii1 = load('/path/to/file1.nii.gz')
+nii2 = load('/path/to/file2.nii.gz')
+nii3 = load('/path/to/file3.nii.gz')
 
 basename = nii1.get_filename().split(os.extsep, 1)[0]
 dirname = os.path.dirname(nii1.get_filename())
@@ -33,16 +33,22 @@ bary = coda.closure(rgb)
 anorm = coda.aitchison_norm(bary)
 
 # prepare reference vector for anglular difference
+# TODO: export for all three main references
 ref = np.ones(bary.shape)
-ref[:, 0] = ref[:, 0] * 0.9
-ref[:, 1] = ref[:, 1] * 0.05
+ref[:, 0] = ref[:, 0] * 0.05
+ref[:, 1] = ref[:, 1] * 0.9
 ref[:, 2] = ref[:, 2] * 0.05
 
 # compute aitchion angular difference
 ainp = coda.aitchison_inner_product(bary, ref)
-ref_norm = coda.aitchison_norm(ref)
 
-idx = bary[:, 1] > bary[:, 2]
+# Export inner product
+img = ainp.reshape(dims[:-1])
+out = Nifti1Image(img, affine=nii1.affine)
+save(out, os.path.join(dirname, 'a_innerp.nii.gz'))
+
+ref_norm = coda.aitchison_norm(ref)
+idx = bary[:, 0] > bary[:, 2]  # NOTE: Selected in relation to the reference
 ang_dif = np.zeros(anorm.shape)
 ang_dif[idx] = np.arccos(ainp[idx]/(anorm[idx] * ref_norm[idx]))
 ang_dif[~idx] = 2*np.pi - np.arccos(ainp[~idx]/(anorm[~idx] * ref_norm[~idx]))
