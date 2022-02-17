83ef7645124793c8daf629333dcc4ce768ccd21d
Omer Faruk Gulban <farukgulban@gmail.com>
2022-02-17 19:02:55 +0100

    wip...

--- examples/high_dimensionals/nearest_balance_axes.py
+++ examples/high_dimensionals/nearest_balance_axes.py
@@ -13,9 +13,24 @@ data = data_dict["rawimg"]
 dims = data.shape
 
 # Remove negative by translation
-data[np.isnan(data)] = 0
-data = data - (data.min() - 1)
+idx_nan = np.isnan(data)
+data[idx_nan] = 0
+
+# data = np.abs(data) + 0.1
+# data = data - (data.min() - 1)
+idx_clip = data < 0
+data[data < 0] = 0
+data += 0.001
 data = data.reshape([dims[0] * dims[1], dims[2]])
+
+# =============================================================================
+# L2 Norm
+norm_L2 = np.sqrt(np.sum(np.power(data, 2), axis=1))
+img = norm_L2 - norm_L2.min()
+img = img / (norm_L2.max() - norm_L2.min()) * 255
+img = np.asarray(img, dtype=("uint8"))
+img = np.reshape(img, dims[0:2])
+cv2.imwrite("/home/faruk/gdrive/thingsonthings/post-compositonal_visuals/test_L2norm.png", img)
 
 # =============================================================================
 # Compute Aitchison norm (aka saturation)
@@ -33,3 +48,41 @@ cv2.imwrite("/home/faruk/gdrive/thingsonthings/post-compositonal_visuals/test_an
 # =============================================================================
 # Compute nearest balance axes
 bary2 = np.sort(bary, axis=1)  # Sort components to simplify the problem
+
+# Reference points in simplex space
+ref = np.asarray(
+    [[1.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [1.0, 1.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [1.0, 1.0, 1.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.1, 0.1],
+     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.1],
+     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1],
+     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1]])
+ref = coda.closure(ref)
+
+# Scaling: make ref points lying on a sphere
+ref_anorm = coda.aitchison_norm(ref)
+ref_scaled = ref_anorm[0, None] / ref_anorm
+ref_scaled = coda.power(ref, ref_scaled[..., None])
+
+# -----------------------------------------------------------------------------
+# Compute distances
+dist = np.zeros((bary2.shape[0], ref.shape[0]))
+for i in range(ref.shape[0]):
+    dist[:, i] = coda.aitchison_dist(bary2, ref_scaled[None, i, :])
+
+# Classification
+idx_min = np.argmin(dist, axis=-1)
+
+# Save output as grayscale
+img = idx_min / (ref.shape[0]-1) * 255
+img = np.asarray(img, dtype=("uint8"))
+img = np.reshape(img, dims[0:2])
+
+img[idx_nan[:, :, 0]] = 0
+img[np.min(idx_clip, axis=-1)] = 0
+
+cv2.imwrite("/home/faruk/gdrive/thingsonthings/post-compositonal_visuals/test_class.png", img)
+print("Finished.")
